4b9a1d6386c14de32bc25230fea9bb91c735ca27
Paulo Jauregui <jauregui.paulo@gmail.com>
2025-10-20 18:26:14 +0000

    working with AppArmor disabled. Pre-PVE migration

--- envs/local-libvirt/outputs.tf
+++ envs/local-libvirt/outputs.tf
@@ -2,3 +2,6 @@ output "hello_vm_ip" {
   description = "IP address of the hello-noble VM"
   value       = try(libvirt_domain.hello.network_interface[0].addresses[0], null)
 }
+output "hello_vm_mac" {
+  value = try(libvirt_domain.hello.network_interface[0].mac, null)
+}
